a4c12c6d60b95b4823a16af4d3175eae2bafa3db
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-09-11 15:56:40 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from bottle import TEMPLATE_PATH, Bottle, debug, redirect, request
+from bottle import jinja2_template as template
+from bottle import run, static_file
+
+
+# -------------------------------------------------
+# Webサーバ設定
+# -------------------------------------------------
+
+app = Bottle()
+debug(debug_mode)
+TEMPLATE_PATH.append("/template")
+
+
+@app.route('/index', method=['GET'])
+def index():
+    return template('index.html)
