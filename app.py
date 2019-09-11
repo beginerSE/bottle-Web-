a5f3e87d4ddd3c7251c93c463b0f31bfc1dda08b
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-09-11 18:33:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from bottle import TEMPLATE_PATH, Bottle, debug, redirect, request
 from bottle import jinja2_template as template
 from bottle import run, static_file
@@ -9,14 +8,13 @@ from bottle import run, static_file
 # -------------------------------------------------
 
 app = Bottle()
-debug(debug_mode)
-TEMPLATE_PATH.append("/template")
+TEMPLATE_PATH.append(
+    "cmd実行してるパスから見た相対パスtemplate")
 
 
-@app.route('/index', method=['GET'])
+@app.route('/', method=['GET'])
 def index():
     return template('index.html', title='sample_page')
-                    
-                    
 
-run(app=app, server='paste', host=host, port=port)
+
+run(app=app)
